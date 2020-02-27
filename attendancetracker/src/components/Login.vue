52d2d79548f8924fffe0d5277ebc54cf397f4ae7
jslightham <31053827+jslightham@users.noreply.github.com>
2020-02-27 11:39:24 -0500

    transfer

---
+++ attendancetracker/src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="login-box">
+      <h1 class="title">Login</h1>
+      <div class="field">
+      <label class="label">Name</label>
+        <div class="control">
+           <input class="input" type="text" placeholder="e.g Alex Smith">
+        </div>
+      </div>
+
+    <div class="field">
+      <label class="label">Email</label>
+      <div class="control">
+        <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com">
+      </div>
+    </div>
+      </div>
+  </template>
+
+  <script>
+    export default {
+        data(){
+        return {
+          post:{}
+        }
+    },
+    methods: {
+      login(){
+          let uri = 'http://localhost:4000/posts/add';
+          this.axios.post(uri, this.post).then(() => {
+          this.$router.push({name: 'posts'});
+      });
+}
+    }
+  }
+</script>
+  
--- attendancetracker/src/main.js
+++ attendancetracker/src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
 
+Vue.use(Buefy)
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+Vue.use(VueAxios, axios);
+Vue.config.productionTip = false;
+
+import HelloWorld from './components/HelloWorld.vue';
+import Login from './components/Login.vue';
+
+const routes = [
+  {
+      name: 'home',
+      path: '/',
+      component: HelloWorld
+  },
+  {
+      name: 'login',
+      path: '/login',
+      component: Login
+  }
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
